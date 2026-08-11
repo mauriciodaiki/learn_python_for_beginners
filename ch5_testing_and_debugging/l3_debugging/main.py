fbb4963937f1ba2a379af30aebc64eba11cf1dda
Mauricio Daiki Rascon Yamamoto <mauriciodaiki@Mac-mini-de-Mauricio.local>
2026-08-11 17:57:15 -0600

    Submit ch5, l3: debugging

--- ch5_testing_and_debugging/l3_debugging/main.py
+++ ch5_testing_and_debugging/l3_debugging/main.py
@@ -1,2 +1,5 @@
-def total_xp(level, xp_to_add):
-    pass
+def take_magic_damage(health, resist, amp, spell_power):
+    total_damage = spell_power * amp
+    actual_damage = total_damage - resist
+    new_health = health - actual_damage
+    return new_health
