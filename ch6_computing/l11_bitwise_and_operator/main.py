d28d438cf6b7bf2cdddd46e4a9a668fc7b4b080e
Mauricio Rascon <mauriciodaiki@outlook.com>
2026-08-16 18:03:43 -0600

    Submit CH6/L11: Bitwise & Operator

--- ch6_computing/l11_bitwise_and_operator/main.py
+++ ch6_computing/l11_bitwise_and_operator/main.py
@@ -5,16 +5,16 @@ can_edit_guild = 0b0001
 
 
 def get_create_bits(user_permissions):
-    pass
+    return user_permissions & can_create_guild
 
 
 def get_review_bits(user_permissions):
-    pass
+    return user_permissions & can_review_guild
 
 
 def get_delete_bits(user_permissions):
-    pass
+    return user_permissions & can_delete_guild
 
 
 def get_edit_bits(user_permissions):
-    pass
+    return user_permissions & can_edit_guild
