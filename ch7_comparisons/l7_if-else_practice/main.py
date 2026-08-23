e470bfd5f7f83a0fd3f93bbde8684eb41a7a9cf9
Mauricio Rascon <mauriciodaiki@outlook.com>
2026-08-22 23:36:11 -0600

    Submit CH7/L7 If-Else Practice

--- ch7_comparisons/l7_if-else_practice/main.py
+++ ch7_comparisons/l7_if-else_practice/main.py
@@ -1,5 +1,5 @@
 def check_high_score(current_player_name, high_scoring_player_name):
-    if True:
+    if current_player_name == high_scoring_player_name:
         return "You are the highest scoring player!"
     else:
         return "You are not the highest scoring player!"
