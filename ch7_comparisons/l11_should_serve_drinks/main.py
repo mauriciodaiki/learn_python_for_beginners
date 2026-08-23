5017388fbb062370df6c8a9aed34c85bf79b147d
Mauricio Rascon <mauriciodaiki@outlook.com>
2026-08-23 03:53:31 -0600

    Submit CH7/L11: Should Serve Drinks

--- ch7_comparisons/l11_should_serve_drinks/main.py
+++ ch7_comparisons/l11_should_serve_drinks/main.py
@@ -1,2 +1,7 @@
 def should_serve_customer(customer_age, on_break, time):
-    pass
+    if customer_age >= 21:
+        if not on_break: 
+            if time >= 5 and time <= 10:
+                return True
+    return False
+
