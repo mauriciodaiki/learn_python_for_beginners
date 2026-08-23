43ace41fd883cb09f5cad92399c8fbbdf546db1a
Mauricio Rascon <mauriciodaiki@outlook.com>
2026-08-23 00:04:08 -0600

    Submit CH7/L8: If-Else Practice

--- ch7_comparisons/l8_if-else_practice/main.py
+++ ch7_comparisons/l8_if-else_practice/main.py
@@ -1,2 +1,7 @@
 def check_high_score(player_name, high_scoring_player_name, low_scoring_player_name):
-    pass
+    if player_name == high_scoring_player_name:
+        return "high"
+    elif player_name == low_scoring_player_name:
+        return "low"
+    else:
+        return "neither"
