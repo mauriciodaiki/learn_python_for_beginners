963263f594f54121e55d089bab359bceed694123
Mauricio Rascon <mauriciodaiki@outlook.com>
2026-09-07 01:54:01 -0600

    Submit CH9/L22: Filter messages

--- ch9_lists/l22_filter_messages/main.py
+++ ch9_lists/l22_filter_messages/main.py
@@ -1,2 +1,18 @@
 def filter_messages(messages):
-    pass
+    filtered_messages = []
+    dang_count = []
+    for message in messages:
+        words = message.split()
+        good_words = []
+        dangs = 0
+        for word in words:
+            if word == "dang":
+                dangs += 1
+            else:
+                good_words.append(word)
+        filtered_message = " ".join(good_words)
+        filtered_messages.append(filtered_message)
+        dang_count.append(dangs)
+    return filtered_messages, dang_count
+
+            
